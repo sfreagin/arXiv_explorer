4539eb0bccce6aca50384d81c8bcbc165e30df1d
Trevor Sauerbrey <trevsauer@gmail.com>
2024-02-22 20:21:34 -0800

    push wordcloud_generator.py update

--- arxiv_app_modules/data/wordcloud_generator.py
+++ arxiv_app_modules/data/wordcloud_generator.py
@@ -2,6 +2,19 @@
 import pandas as pd
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+
+def create_custom_colormap():
+    # Define the colors for the custom colormap
+    colors = ['black', 'firebrick', 'blue']  # black, red, blue
+
+    # Define the boundaries for each color
+    bounds = [0, 0.2, 0.8, 1]
+
+    # Create a colormap object using ListedColormap
+    cmap = mcolors.ListedColormap(colors)
+
+    return cmap, bounds
 
 def generate_wordcloud_from_csv(csv_file):
     # Read the csv file into a DataFrame
@@ -14,17 +27,22 @@ def generate_wordcloud_from_csv(csv_file):
     # Concatenate all summaries into a single string
     text = ' '.join(df['Summary'].dropna())
     
+    # Create the custom colormap
+    custom_cmap, bounds = create_custom_colormap()
+    
     # Generate a word cloud image
-    wordcloud = WordCloud(width = 800, height = 800, 
-                background_color ='white', 
-                stopwords = None, 
-                min_font_size = 10).generate(text)
+    wordcloud = WordCloud(width=800, height=800, 
+                          background_color='white', 
+                          colormap=custom_cmap,
+                          contour_color='black',
+                          contour_width=2,
+                          min_font_size=10).generate(text)
     
     # Display the generated image:
-    plt.figure(figsize = (8, 8), facecolor = None)
-    plt.imshow(wordcloud)
+    plt.figure(figsize=(8, 8), facecolor=None)
+    plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis("off")
-    plt.tight_layout(pad = 0)
+    plt.tight_layout(pad=0)
     
     # Save the word cloud image to a file
     wordcloud.to_file("wordcloud_output.png")
